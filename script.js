ff39abb1592a99014193b02c322428b40f4eee21
Yuliia00 <skaskevich2000@gmail.com>
2023-02-18 16:54:28 +0100

    add_task1_task2

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let str1 = prompt("Input your 1st string",);
 let str2 = prompt("Input your 2nd string",);
 let str3 = prompt("Input your 3rd string",);
 alert(`You entered: ${str1} ${str2} ${str3}`);
-
 //task 2
 let number = prompt('Please enter 5 digit number', '');
 if (number.length !== 5) {
